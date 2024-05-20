58cef9239129147f38434dadf5bfc940d54b3d8b
GodDennis <denpodreckiy@mail.ru>
2024-05-20 16:18:40 +0300

    fex

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <HashRouter>
-        <Routes>
-            <Route
-                path='/'
-                element={<App />}
-            />
-        </Routes>
-    </HashRouter>
+    <React.StrictMode>
+        <HashRouter>
+            <Routes>
+                <Route
+                    path='/'
+                    element={<App />}
+                />
+            </Routes>
+        </HashRouter>
+    </React.StrictMode>
 );
